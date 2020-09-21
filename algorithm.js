47c966c870b4bc7d5192c5f610ef2d3dc483c99a
BB <seven.albany.bi@gmail.com>
2020-09-21 18:50:36 +0800

    algorithm test cases passed

--- algorithm.js
+++ algorithm.js
@@ -1,24 +1,35 @@
 function step_color(step, color_range) {
 	const COLOR_RANGE = 256
+
 	// create normal color range
-	for (var i = 0; i < COLOR_RANGE; i++) {
+	for(var i = 0; i < COLOR_RANGE; i++) {
 		color_range.push(i)
 	}
-
 	// generate color range by giving `s` step
-	return color_range.filter(item => item % step == 0)
+	return color_range.filter(item => item % step === 0)
 }
 
-function rgb_picker(color_range, res, start) {
-	
+
+function rgb_picker(color_range, index_stack, index, res) {
+	if(index >= 3) {
+		let temp = [...index_stack]
+		res.push(temp)
+		return
+	}
+
+	for (var i = 0; i < color_range.length; i++) {
+		index_stack.push(color_range[i])
+		rgb_picker(color_range, index_stack, index+1, res)
+		index_stack.pop()
+	}
 }
 
 function algorithm(step) {
-	let my_color_range = step_color(step, [])
+	let my_color_range = step_color(step, []) //color range according to gave steps
 	let res = [] // my color combinations result
 	let start = 0
-
-	rgb_picker(my_color_range, res, start)
+	// pick all possible colors
+	rgb_picker(my_color_range, [], 0, res)
 
 	return res
 }
--- algorithm.test.js
+++ algorithm.test.js
@@ -19,4 +19,28 @@ describe('Test Algorithm', () => {
 	test('-> step_color with step 8 and expect array return', () => {
 		expect(algorithm.step_color(8, [])).toEqual(expect_return)
 	});
+
+	const sample_color_range = [
+		0,   8,  16,  24,  32,  40,  48,  56,                                                                                                                                                                         
+		64,  72,  80,  88,  96, 104, 112, 120,                                                                                                                                                                          
+		128, 136, 144, 152, 160, 168, 176, 184,                                                                                                                                                                         
+		192, 200, 208, 216, 224, 232, 240, 248
+	]
+	const expect_len = 32*32*32
+	let res = []
+	algorithm.rgb_picker(sample_color_range, [], 0, res)
+	test('-> rgb_picker with sample color range (length 32), return expect length of combinations', () => {
+		expect(res.length).toEqual(expect_len)
+	});
+
+	const sample_color_range2 = [                                                                                                                                                                           
+		64,  72,  80,  88,  96,                                                                                                                                                                        
+		128, 136, 144, 152, 160                                                                                                                                                   
+	]
+	const expect_len_2 = 10*10*10
+	let res2 = []
+	algorithm.rgb_picker(sample_color_range2, [], 0, res2)
+	test('-> rgb_picker with sample color range (length 10), return expect length of combinations', () => {
+		expect(res2.length).toEqual(expect_len_2)
+	});		
 });
